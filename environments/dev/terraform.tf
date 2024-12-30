62e00d42e6c0730d7857c6e961aa3fd1b22722b8
kwit75 <dmitry.karataev@gmail.com>
2024-12-29 23:54:46 -0800

    phase2

--- environments/dev/terraform.tf
+++ environments/dev/terraform.tf
@@ -2,11 +2,11 @@ terraform {
   # required_version = "1.9.7"
 
   # where to store state files
-  backend "s3" {
+backend "s3" {
     bucket         = "mediaviz-terraform-backend-config-dev"
     key            = "terraform.tfstate"
     region         = "us-east-2"
-    dynamodb_table = "terraform-state-lock"  # Add this line for state locking
+    dynamodb_table = "mediaviz-terraform-backend-config-dev"  # Changed to match your existing table
     encrypt        = true
   }
 
