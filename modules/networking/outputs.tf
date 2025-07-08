87c704a1d7e92f65a573a3912f6269c77666ec91
steven <steven.harrison@gmail.com>
2025-07-08 19:07:00 -0400

    Or this output

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -14,10 +14,10 @@ output "public_subnets" {
 }
 
 # Aurora will use the /24 subnets (indices 3, 4, 5)
-output "aurora_subnets" {
+/*output "aurora_subnets" {
   description = "The IDs of the private subnets for Aurora (/24 subnets)"
   value       = slice(module.vpc.private_subnets, 3, 6)
-}
+}*/
 
 # EKS will use the /22 subnets (indices 0, 1, 2)
 output "eks_subnets" {
