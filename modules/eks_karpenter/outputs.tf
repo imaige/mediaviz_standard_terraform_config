8f0c86190dbc467384a5395b0ccd3d05cba6ec9d
steven <steven.harrison@gmail.com>
2025-07-08 19:06:51 -0400

    We didn't need this output

--- modules/eks_karpenter/outputs.tf
+++ modules/eks_karpenter/outputs.tf
@@ -34,11 +34,11 @@ output "node_security_group_id" {
   value       = module.eks.node_security_group_id
 }
 
-output "node_group_role_arn" {
+/*output "node_group_role_arn" {
   description = "IAM role ARN for the node groups"
   value       = module.eks.eks_managed_node_groups["primary"].iam_role_arn
 }
-
+*/
 output "cluster_certificate_authority_data" {
   description = "Base64 encoded certificate data required to communicate with the cluster"
   value       = module.eks.cluster_certificate_authority_data # If using official eks module
